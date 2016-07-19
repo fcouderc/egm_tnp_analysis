bbcd19753cde4cb487345eb563123dfa724529d0
Fabrice Couderc <fabrice.couderc@cern.ch>
2016-07-19 13:35:54 +0200

    change setup file name

---
+++ etc/scripts/setup.sh
@@ -0,0 +1,18 @@
+#!bin/bash 
+
+if [[ `uname -n` == *"lxplus"* ]]; then 
+## install gcc 4.9
+    optVer=x86_64-slc6-gcc49-opt
+    optVer=x86_64-slc6-gcc48-opt
+
+## install modern version of ROOT
+    LCG=/afs/cern.ch/sw/lcg/
+    ROOTSYS=$LCG/app/releases/ROOT/6.06.06/$optVer/root/
+    . $LCG/external/gcc/4.8.0/$optVer/setup.sh
+    . $ROOTSYS/bin/thisroot.sh
+
+## add python lib
+    . /opt/rh/python27/enable
+fi
+
+export PYTHONPATH=.:$PYTHONPATH
