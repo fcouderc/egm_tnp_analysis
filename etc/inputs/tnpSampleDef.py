cf799cdb3b73fd6abe859cf1a404842c788d2145
Fabrice Couderc <fabrice.couderc@cern.ch>
2016-07-25 19:42:39 +0200

    add Wjets sample

--- etc/inputs/tnpSampleDef.py
+++ etc/inputs/tnpSampleDef.py
@@ -9,8 +9,11 @@ ICHEP2016 = {
     'mc_DY_amcatnlo_ele' : tnpSample('mc_DY_amcatnlo_ele', eosDir + 'Photons_76Xids/elev2/mc/TnPTree_DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8.root', isMC = True, nEvts = 28696958 ),
     'mc_DY_madgraph_pho' : tnpSample('mc_DY_madgraph_pho', eosDir + 'Photons_76Xids/phov2/mc/TnPTree_DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8.root' , isMC = True, nEvts = 41253879 ),
     'mc_DY_amcatnlo_pho' : tnpSample('mc_DY_amcatnlo_pho', eosDir + 'Photons_76Xids/phov2/mc/TnPTree_DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8.root', isMC = True, nEvts = 28696958 ),
+    'mc_Wjets_madgraph_pho' : tnpSample('mc_Wjets_madgraph_pho', eosDir + 'Photons_76Xids/phov2/mc/WJetsToLNu_TuneCUETP8M1_13TeV-madgraphMLM-pythia8.root'        , isMC = True, nEvts = 999 ),
     'mc_DY_madgraph_rec' : tnpSample('mc_DY_madgraph_rec', eosDir + 'RecoSF/RECOv3/mc//TnPTree_DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8.root'       , isMC = True, nEvts = 49877138 ),
     'mc_DY_amcatnlo_rec' : tnpSample('mc_DY_amcatnlo_rec', eosDir + 'RecoSF/RECOv3/mc//TnPTree_DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8.root'      , isMC = True, nEvts = 28328619 ),
+
+
 
     'data_2016_runB_ele' :  tnpSample('data_2016_runB_ele', eosDir + 'Photons_76Xids/elev2/data/TnPTree_SingleElectron_2016_RunB.root' , lumi = 5.709 ),
     'data_2016_runC_ele' :  tnpSample('data_2016_runC_ele', eosDir + 'Photons_76Xids/elev3/data/TnPTree_SingleElectron_2016_RunC.root' , lumi = 2.646 ),
