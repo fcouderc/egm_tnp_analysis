27713a7951555d3bb5089f44ad7b0c15c4e56efa
Fabrice Couderc <fabrice.couderc@cern.ch>
2016-12-07 22:39:19 +0100

    update with most of syst

--- etc/scripts/tnpFitterAllSteps.sh
+++ etc/scripts/tnpFitterAllSteps.sh
@@ -20,8 +20,8 @@ for flag in ${flags[@]}; do
     python tnpEGM_fitter.py  $script --flag $flag --createBins
     python tnpEGM_fitter.py  $script --flag $flag --createHists
     python tnpEGM_fitter.py  $script --flag $flag --doFit
-#python tnpEGM_fitter.py  $script --flag $flag --doFit --mcSig --altSig
-#python tnpEGM_fitter.py  $script --flag $flag --doFit --altSig
+    python tnpEGM_fitter.py  $script --flag $flag --doFit --mcSig --altSig
+    python tnpEGM_fitter.py  $script --flag $flag --doFit --altSig
     python tnpEGM_fitter.py  $script --flag $flag --doFit --altBkg
     python tnpEGM_fitter.py  $script --flag $flag --sumUp
 done
